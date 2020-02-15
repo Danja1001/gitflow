662c28cdf9b525957cd5430974d748ec932b4b38
Danilo <brickk1337@gmail.com>
2020-02-15 10:19:55 +0200

    change hello message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Hello, {{ auth()->user()->name }}!
                 </div>
             </div>
         </div>
